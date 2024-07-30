8021e6f28ff7d8b55409af694988972eefb01ff0
2433924494 <2433924494@qq.com>
2024-07-30 17:05:25 +0800

    Optimize

--- Client_Application.py
+++ Client_Application.py
@@ -5,13 +5,32 @@ import base64
 import json
 import argparse
 from aip import AipFace
+from threading import Thread
+from apscheduler.schedulers.blocking import BlockingScheduler
+import logging
 
-PROTOTXT_PATH='./deploy_prototxt.txt'
+with open('../Baidu_keys.json','r') as fp:
+    keys=json.load(fp)
+with open('../room_info.json','r') as fp:
+    room_info=json.load(fp) 
+imageType = "BASE64"
+groupIdList=room_info['room_id']
+options={
+    'liveness_control':'NORMAL',
+    'match_threshold':80
+}   
+logging.basicConfig(filename='./logs.txt',level=logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',encoding='utf-8')
+PROTOTXT_PATH='./models/deploy_prototxt.txt'
 MODEL_PATH='./models/res10_300x300_ssd_iter_140000_fp16.caffemodel'
 model = cv2.dnn.readNetFromCaffe(PROTOTXT_PATH, MODEL_PATH)
+
+
 def run():
     cap = cv2.VideoCapture(args.video_device)
+    start_time=time.time()
     while True:
+        if time.time()-start_time>15:
+            break
         ret, frame = cap.read()
         if not ret:
             print('cap error,system exit')
@@ -25,8 +44,10 @@ def run():
             If_Live=response['If_Live']
             # draw_name(face_detect,frame)
             print(f'姓名:{face_name} 活体:{True if If_Live==1 else False}')
+            logging.info(f'姓名:{face_name} 活体:{True if If_Live==1 else False}')
         else:
             print('None')
+            logging.warning('No faces!')
         # 展示视频画面
         cv2.imshow('video capture', frame)
         key = cv2.waitKey(10) & 0xff
@@ -37,48 +58,52 @@ def run():
 
 
 
-
+def req_baidu(client:AipFace,frame,):
+    
+    img_base64=image_to_base64(frame)
+    response=client.multiSearch(img_base64,imageType,groupIdList,options)
+    # print(response)
+    
+    if response["error_msg"]=='SUCCESS':
+        result=response['result']
+        face=result['face_list'][0]
+        # if face:
+        #     x=int(face["location"]['left'])
+        #     y=int(face["location"]['top'])
+        #     w=int(face["location"]['width'])
+        #     h=int(face["location"]['height'])
+        #     cv2.rectangle(frame,(x,y),(x+w,y+h),color=(255,0,0),thickness=2)
+        print(f"Name:{face['user_list'][0]['user_info']} Score:{face['user_list'][0]['score']:.2f}")
+        logging.info(f"Name:{face['user_list'][0]['user_info']}")
+        return True
+    else:
+        # print('Name:Unknown')
+        logging.warning(f'Request API failed! Erro:{response["error_msg"]}')
+        return False
 def run_Baidu():
     cap=cv2.VideoCapture(args.video_device)
-    with open('../Baidu_keys.json','r') as fp:
-        keys=json.load(fp)
+    
     client=AipFace(keys['APP_ID'],keys['API_KEY'],keys['SECRET_KEY'])
-    imageType = "BASE64"
-    groupIdList=args.room
-    options={
-        'liveness_control':'NORMAL',
-        'match_threshold':70
-    }   
+    
+    # camara_display(cap=cap)
+    start_time=time.time()
     while True:
+        if time.time()-start_time>15:
+            break
         ret,frame=cap.read()
         frame = cv2.flip(frame, 1)
-        # face_exist,face_ROI=Get_Face(frame)
-        
-        img_base64=image_to_base64(frame)
-        response=client.multiSearch(img_base64,imageType,groupIdList,options)
-        # print(response)
-        
-        if response["error_msg"]=='SUCCESS':
-            result=response['result']
-            face=result['face_list'][0]
-            if face:
-                x=int(face["location"]['left'])
-                y=int(face["location"]['top'])
-                w=int(face["location"]['width'])
-                h=int(face["location"]['height'])
-                cv2.rectangle(frame,(x,y),(x+w,y+h),color=(255,0,0),thickness=2)
-                print(f"Name:{face['user_list'][0]['user_info']} Score:{face['user_list'][0]['score']:.2f}")
-            else:
-                print('Name:Unknown')
-        else:
-            print(f'Request API failed!\nErro:{response["error_msg"]}')
+        draw_face(frame,dnn_face_detect(frame))
+        if req_baidu(client=client,frame=frame):
+            break
+        # 测试用
+        # time.sleep(1)
         cv2.imshow('video capture', frame)
-        time.sleep(1)
         key = cv2.waitKey(10) & 0xff
         if key == 27:
             cap.release()
             cv2.destroyAllWindows()
             break
+    
 import numpy as np
 def get_ROI(x,y,w,h,frame):
     block_len=max(w,h)
@@ -90,43 +115,35 @@ def get_ROI(x,y,w,h,frame):
     # print(f'ROI_point:{x,y,x1-1,y1-1}  Rec+point{x,y,x+w,y+h}')
     cv2.imshow('ROI', ROI)
     return ROI
-def Get_Face(frame):
-    # face_detector = cv2.CascadeClassifier('./face_feature_detect/haarcascade_frontalface_default.xml')
-    # grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # faces = face_detector.detectMultiScale(grey)
-    # print('Exist' if faces.any() else 'No faces')
-    h,w=frame.shape[:2]
+
+def dnn_face_detect(frame):
     blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
     model.setInput(blob)
     faces=np.squeeze(model.forward())
-    Max_face_len=-1
-    Max_face_index=-1
+
+    return faces
+
+def Get_Face(frame):
     ori_img=frame.copy()
-    Face_pos=[]
-    cnt=-1
-    for i in range(0, faces.shape[0]):
-        confidence = faces[i, 2]
-        if confidence > 0.5:
-            cnt+=1
-            # print(faces[i, 3:7].shape)
-            # print(f'w:{w}h:{h}')
-            # print(np.array([w, h, w, h]).shape)
-            box = faces[i, 3:7] * np.array([w, h, w, h])
-            start_x, start_y, end_x, end_y = box.astype(int)
-            Face_pos.append([start_x,start_y,end_x-start_x+1,end_y-start_y+1])
-            cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), color=(255, 0, 0), thickness=2)
-            if max((end_x-start_x),(end_y-start_y))>Max_face_len:
-                Max_face_len=max((end_x-start_x),(end_y-start_y))
-                Max_face_index=cnt
-    # for i,(x, y, w, h) in enumerate(faces):
-	# # 在人脸区域绘制矩形
-    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
-    #     # ROI.append(get_ROI(x,y,w,h,frame))
-    #     if max(w,h)>Max_face_len:
-    #         Max_face_len=max(w,h)
-    #         Max_face_index=i
-    if Max_face_index>-1:
-        return True,get_ROI(*(Face_pos[Max_face_index]),ori_img)
+    h,w=frame.shape[:2]
+    faces=dnn_face_detect(frame)
+    # 为人脸画框
+    draw_face(faces=faces,frame=frame)
+    max_len=0
+    max_index=-1
+    for i in range(faces.shape[0]):
+        confidence=faces[i,2]
+        if confidence>0.5:
+            start_x, start_y, end_x, end_y=faces[i,3:7]
+            len=max(end_x-start_x,end_y-start_y)
+            if len>max_len:
+                max_index=i
+    box = faces[max_index, 3:7] * np.array([w, h, w, h])
+    start_x, start_y, end_x, end_y = box.astype(int)
+    Face_pos=[start_x,start_y,end_x-start_x+1,end_y-start_y+1]
+    
+    if max_index>-1:
+        return True,get_ROI(*(Face_pos),ori_img)
     else:
         return False,None
 def draw_name(name,frame,pos):
@@ -134,6 +151,15 @@ def draw_name(name,frame,pos):
     # fontpath = "simsun.ttc"
     pass
 
+def draw_face(frame,faces):
+    h,w=frame.shape[:2]
+    for i in range(0, faces.shape[0]):
+        confidence = faces[i, 2]
+        if confidence>0.5:
+            box = faces[i, 3:7] * np.array([w, h, w, h])
+            start_x, start_y, end_x, end_y = box.astype(int)
+            cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), color=(255, 0, 0), thickness=2)
+    
 def image_to_base64(image_np):
     image = cv2.imencode('.jpg', image_np)[1]
     image_code = str(base64.b64encode(image))[2:-1]
@@ -150,9 +176,9 @@ def detect_face(face):
 parser=argparse.ArgumentParser()
 parser.add_argument('-m',dest='mode',type=int,default=1,help='0/1 Baidu-api or Server-api')
 parser.add_argument('-vd',dest='video_device',type=int,default=0,help='video device number')
-parser.add_argument('-r',dest='room',type=str,help='room id e.g. 08_1803B ->(栋号)_(寝室号)')
 args=parser.parse_args()
 if __name__ == '__main__':
+    
     if args.mode:
         run()
     else:
--- Server_Application.py
+++ Server_Application.py
@@ -1,28 +1,17 @@
 import os
-import time
 import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
 from models.facenet import Facenet
-from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 from pydantic import BaseModel
-import json
 import uvicorn
 import cv2
 import numpy as np
-import io
 import base64
 import Live_Detection.detect as LD
 
 
 app = FastAPI()
-device = select_device('')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 half = device.type != 'cpu'  # half precision only supported on CUDA
 
 
